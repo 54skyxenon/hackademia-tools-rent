438ccf5c58cc3a3f0cef36e73c87982b78db9081
54skyxenon <54skyxenon@gmail.com>
2018-07-28 17:49:59 -0700

    removed django backend

--- django_backend/django_backend/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django_backend.views import *
-
-urlpatterns = [
-    path('', HomeView),
-]
--- django_backend/django_backend/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def HomeView(request):
-	return render(request, 'django_backend/home.html')
